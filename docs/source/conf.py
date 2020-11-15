7c9cac128487fa0c7b99b76e566349fb3d68436d
W Zhang <wz315@cam.ac.uk>
2020-11-15 12:04:04 +0000

    ci: Build and deploy Sphinx docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,6 +24,8 @@ author = 'Weixuan Zhang'
 
 # -- General configuration ---------------------------------------------------
 
+master_doc = 'index'
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
@@ -50,3 +52,11 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+# -- Options for LaTeX output ------------------------------------------------
+
+latex_documents = [
+    (master_doc, 'materials-modelling.tex', 'Materials Modelling',
+        author, 'manual', False)
+]
